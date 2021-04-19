a027e8bc957ef586015a1bdc060ca9a2f7867066
faivrech <christophe.faivre@natixis.com>
2021-04-19 12:52:52 +0200

    intial commit

---
+++ src/main/java/fr/wildcodeschool/unittesting/StringUtils.java
@@ -0,0 +1,49 @@
+package fr.wildcodeschool.unittesting;
+
+import java.util.*;
+
+public class StringUtils {
+
+    private static final String ALL_VOWELS = "aeiouyAEIOUY";
+
+    /**
+     * Renvoie la chaine formée par les voyelles d'une chaine de caractères
+     * @return Chaine avec uniquement des voyelles
+     */
+    public static String vowels(String candidate) {
+        String vowels = "";
+        if (candidate != null) {
+            char[] letters = candidate.toCharArray();
+            for (int i = 0; i < candidate.length(); i++) {
+                if (ALL_VOWELS.indexOf(letters[i]) >= 0) {
+                    vowels += letters[i];
+                }
+            }
+        }
+        return vowels;
+    }
+
+    public static String uniqueVowels(String candidate) {
+        String uniqueVowels = "";
+        char[] letters = candidate.toCharArray();
+        ArrayList<Character> uniqueLetters = new ArrayList();
+        boolean present = false;
+        for (int i = 0; i < candidate.length(); i++) {
+            present = false;
+            for (int z = 0; z < uniqueLetters.size(); z++) {
+                Character letter = letters[i];
+                Character uniqueLetter = uniqueLetters.get(z);
+                if (letter.toString().toUpperCase().equals(uniqueLetter.toString().toUpperCase())) {
+                    present = true;
+                }
+            }
+            if (!present) {
+                uniqueLetters.add(letters[i]);
+                uniqueVowels += letters[i];
+            }
+        }
+
+        return uniqueVowels;
+    }
+
+}
---
+++ src/test/java/fr/wildcodeschool/unittesting/StringUtilsTest.java
@@ -0,0 +1,38 @@
+package fr.wildcodeschool.unittesting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringUtilsTest {
+
+    @Test
+    public void testParameterNull() {
+        Assert.assertTrue("failure - should be true", true);
+        Assert.assertNotNull("should not be null", StringUtils.vowels(""));
+        Assert.assertNotNull("should not be null", StringUtils.vowels(null));
+    }
+
+    @Test
+    public void testVowels() {
+
+        Assert.assertTrue("failure - should be true", true);
+        Assert.assertEquals(StringUtils.vowels("voyelle"),"oyee");
+        Assert.assertEquals(StringUtils.vowels("VOYELLE"),"OYEE");
+        Assert.assertEquals(StringUtils.vowels("gnmt"),"");
+        Assert.assertEquals(StringUtils.vowels("oUuy"),"oUuy");
+        Assert.assertEquals(StringUtils.vowels("aeiouy"),"aeiouy");
+        Assert.assertEquals(StringUtils.vowels("Terminer"),"eie");
+
+
+    }
+
+    @Test
+    public void testUniqueVowels() {
+        Assert.assertTrue("failure - should be true", true);
+        Assert.assertEquals(StringUtils.uniqueVowels("aeeeEEEiooOuUy"),"aeiouy");
+        Assert.assertEquals(StringUtils.uniqueVowels("AeIaEiAEIaei"),"AeI");
+        Assert.assertEquals(StringUtils.uniqueVowels("aAEeeeyEEEiIIooOuUYYYy"),"aEyiou");
+    }
+
+
+}
